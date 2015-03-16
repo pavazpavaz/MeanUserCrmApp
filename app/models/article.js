d860110b19885ce8049f293612f736202ba36e1d
pavazpavaz <pavaz27@gmail.com>
2015-03-16 17:11:12 +0000

    added article schema and model

---
+++ app/models/article.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Pedro on 16-03-2015.
+ */
+
+// grab the packages that we need for the message model
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// article schema
+var ArticleSchema = new Schema({
+    author : { type:mongoose.Schema.Types.ObjectId, ref: "User" },
+    body : String
+});
+
+// return the model
+module.exports = mongoose.model("Article", ArticleSchema);
